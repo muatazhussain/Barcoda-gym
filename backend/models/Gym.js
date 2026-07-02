f8676f7e2a46bb87d213483c52534c04b24e158f
الشريف معتز <muataz.h.m@gmail.com>
2026-07-02 03:54:48 +0300

    models: Create Gym model

---
+++ backend/models/Gym.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Gym = sequelize.define('Gym', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: 'صالة رياضية'
+  },
+  owner: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  address: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  logo: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  }
+}, {
+  timestamps: true,
+  tableName: 'gyms'
+});
+
+module.exports = Gym;
