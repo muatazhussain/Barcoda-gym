62d6fa2ffcea4f993dcf853e136e447fe24a4c84
الشريف معتز <muataz.h.m@gmail.com>
2026-07-02 03:54:25 +0300

    models: Create Admin model

---
+++ backend/models/Admin.js
@@ -0,0 +1,63 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const bcrypt = require('bcryptjs');
+
+const Admin = sequelize.define('Admin', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  gymId: {
+    type: DataTypes.UUID,
+    allowNull: false
+  },
+  fullName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  role: {
+    type: DataTypes.ENUM('owner', 'admin'),
+    defaultValue: 'admin'
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  },
+  lastLogin: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+}, {
+  timestamps: true,
+  tableName: 'admins',
+  hooks: {
+    beforeCreate: async (admin) => {
+      admin.password = await bcrypt.hash(admin.password, 10);
+    }
+  }
+});
+
+Admin.prototype.comparePassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = Admin;
